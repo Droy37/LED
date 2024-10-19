7d7d38a0a8912e5c886724a2808bdca8411218a8
Droy37 <catherine.cao37@gmail.com>
2024-10-19 20:32:29 +0800

    upload

---
+++ cmake-build-debug-mingw-stm32/CMakeFiles/test2.elf.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/CLionProjects/test2/Core/Startup/startup_stm32f427iihx.s" "C:/CLionProjects/test2/cmake-build-debug-mingw-stm32/CMakeFiles/test2.elf.dir/Core/Startup/startup_stm32f427iihx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F427xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/CLionProjects/test2/Core/Inc"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "C:/CLionProjects/test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "C:/CLionProjects/test2/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/CLionProjects/test2/Core/Src/dma.c" "CMakeFiles/test2.elf.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/dma.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/gpio.c" "CMakeFiles/test2.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/gpio.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/main.c" "CMakeFiles/test2.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/main.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/stm32f4xx_it.c" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/syscalls.c" "CMakeFiles/test2.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/sysmem.c" "CMakeFiles/test2.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/system_stm32f4xx.c" "CMakeFiles/test2.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "C:/CLionProjects/test2/Core/Src/tim.c" "CMakeFiles/test2.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/tim.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "C:/CLionProjects/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
